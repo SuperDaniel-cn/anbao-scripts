f4429f40da260609d6bcbe282375f3ccdeaaac94
Daniel <superdaniel_cn@outlook.com>
2025-10-03 22:48:42 +0800

    修正

--- com.example.hello-world/1.0.0/bundle.js
+++ com.example.hello-world/1.0.0/bundle.js
@@ -1,12 +1,16 @@
-// ==UserScript==
+// ==AnbaoScript==
 // @id             com.example.hello-world
 // @name           Hello World
 // @version        1.0.0
 // @description    一个简单的脚本，用于在控制台打印 'Hello World'。
-// @author         Anbao Team
+// @author         Daniel
 // @tags           example, hello, test
 // @changelog      初始版本。
-// ==/UserScript==
+// @keywords    baidu,google,sogou,duckduckgo,bing
+// @engine      patchright
+// @launchOptions {"channel":"chrome","headless":false,"slowMo":50}
+//
+// ==/AnbaoScript==
 
 (async function () {
   console.log("你好，世界！这是一个来自安宝脚本市场的脚本。");
